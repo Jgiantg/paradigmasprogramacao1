fd83a730243286fca509b1fe170e0560df548c2b
João Gabriel <joaogabrielmartinsalmeida35@gmail.com>
2025-02-25 19:28:55 -0300

    primeiro site

--- Lista1/src/Carro.java
+++ Lista1/src/Carro.java
@@ -1,17 +1,16 @@
 import javax.swing.*;
 
 public class Carro {
-    // varíaveis
-    // do tipo primitivo
+    // variáveis
+    // cujo tipo de dados são primitivos
     public int ano;
     public float velocidade;
-    // do tipo classe
+    // cujo tipo de dados são Classes
     public String marca, modelo;
-
-    // métodos construtores
-    public Carro(){ // sem parâmetros
-        this.marca = "Marca inválida";
-        this.modelo = "Modelo inválido";
+    // método construtor
+    public Carro(){
+        this.marca = "marca não definida";
+        this.modelo = "modelo não definido";
     }
     public Carro(int ano, float velocidade, String marca, String modelo){
         this.ano = ano;
@@ -19,29 +18,28 @@ public class Carro {
         this.marca = marca;
         this.modelo = modelo;
     }
+    // método de instância
 
-    // métodos
     public void exibirDetalhes(){
-        // this representa o objeto que chama o método
-        JOptionPane.showMessageDialog(null,
-                "Marca: " + this.marca +
-                " Modelo " + this.modelo +
-                " Ano: " + this.ano +
-                " Velocidade " + this.velocidade);
+        // this representa o objeto que chama o método
+        JOptionPane.showMessageDialog(null, "Marca: " + this.marca +
+                "\n Modelo: " + this.modelo +
+                "\n Ano: " + this.ano +
+                "\n Velocidade: " + this.velocidade);
     }
 
-    // vai acelerar o carro de X unidades
+    // acelerar o carro de x unidades
     public void acelerar(float x){
         this.velocidade = this.velocidade + x;
     }
+    // frear o carro em x unidades
+    // velocidade não pode ficar negativa após a freagem
     public void frear(float x){
-        if (this.velocidade - x >= 0){
+        if (this.velocidade >= x){
             this.velocidade -= x;
         }
         else {
-            System.out.println("Não é possível frear");
+            JOptionPane.showMessageDialog(null, "Velocidade não pode ser negativa");
         }
     }
-
-
 }
--- Lista1/src/Main.java
+++ Lista1/src/Main.java
@@ -1,26 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        // criando objetos da classe
-        // instanciar a classe, criando objetos
-        Carro obj1 = new Carro(); // chama o construtor
-        obj1.ano = 2022;
-        obj1.velocidade = 0;
-        obj1.marca = "Volkswagen";
-        obj1.modelo = "Polo";
+        // criação dos objetos da classe Carro
+        // instanciando a classe Carro
+        Carro obj1 = new Carro();
+        obj1.ano = Integer.parseInt(JOptionPane.showInputDialog("Ano"));;
+        obj1.velocidade = Float.parseFloat(JOptionPane.showInputDialog("Velocidade"));;
+        obj1.marca = JOptionPane.showInputDialog("Marca");
+        obj1.modelo = JOptionPane.showInputDialog("Modelo");
         obj1.exibirDetalhes();
-        obj1.acelerar(100);
-        obj1.frear(40);
+        obj1.acelerar(50);
+        obj1.frear(10);
         obj1.exibirDetalhes();
 
-        Carro obj2 = new Carro(2024, 50, "Fiat", "FastBack");
+        Carro obj2 = new Carro(2023, 30, "Fiat", "Argo");
         obj2.exibirDetalhes();
-        obj2.acelerar(30);
+        obj2.acelerar(60);
         obj2.frear(100);
         obj2.exibirDetalhes();
 
-        Carro obj3 = new Carro(); // chama o construtor
+        Carro obj3 = new Carro();
         obj3.exibirDetalhes();
     }
 }
